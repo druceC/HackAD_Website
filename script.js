674ea950e5bad48175f391b30c6feee3097c473b
Druce Chua <89497828+druceC@users.noreply.github.com>
2024-02-21 11:41:07 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
-// let yearSemesterList = ['Spring 2018', 'Fall 2018', 'Spring 2019', 'Fall 2019', 'Spring 2020', 'Fall 2020', 'Spring 2021', 'Fall 2021', 'Spring 2022', 'Fall 2022'];
-let yearSemesterList = ['Fall 2022','Spring 2022','Fall 2021','Spring 2021','Fall 2020','Spring 2020', 'Fall 2019', 'Spring 2019','Fall 2018', 'Spring 2018'];
+let yearSemesterList = ['Spring 2024','Fall 2023','Spring 2023','Fall 2022','Spring 2022','Fall 2021','Spring 2021','Fall 2020','Spring 2020', 'Fall 2019', 'Spring 2019','Fall 2018', 'Spring 2018'];
 let count = 0;
 let countLeftBtn = 0;
 let i = 0;
 let txt = 'Past Events'; /* The text */
+let contact_txt = 'Be Connected!';
 let speed = 70; /* The speed/duration of the effect in milliseconds */
 
-function typeWriter() {
-    if (i < txt.length) {
-        document.getElementById("past-event-heading").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
+function typeWriter(txt, elementId) {
+    let j = 0;
+    let interval = setInterval(() => {
+        if (j < txt.length) {
+            document.getElementById(elementId).innerHTML += txt.charAt(j);
+            j++;
+        } else {
+            clearInterval(interval);
+        }
+    }, speed);
+}
+
+function highlightMenuItem(event) {
+    // Remove highlight class from previously clicked menu item
+    // / Remove font color from the previously selected menu item
+    var previousSelectedItem = document.querySelector('.selected');
+    if (previousSelectedItem) {
+        previousSelectedItem.classList.remove('selected');
     }
+
+    // Change font color of the newly selected menu item to white
+    var selectedItem = event.target;
+    selectedItem.classList.add('selected');
 }
 
 window.addEventListener('load', () => {
     let rightBtn = document.getElementById('right-arrow-btn');
     let leftBtn = document.getElementById('left-arrow-btn');
     let yearTxt = document.getElementById('year-semester-heading');
-    // let child = document.getElementById('events-spring2018');
-    let child = document.getElementById('events-fall2022');
+    let child = document.getElementById('events-fall2021');
     let parent = child.parentNode;
     let children = parent.children;
 
@@ -43,5 +59,8 @@ window.addEventListener('load', () => {
         children[count].style.display = 'block';
     })
 
-    typeWriter();
+    // typeWriter();
+    typeWriter(txt, "past-event-heading");
+    typeWriter(contact_txt, "contact-heading");
 })
+
